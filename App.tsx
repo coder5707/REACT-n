dd97e3428a8b3d60c516354eb4010f97243cd9d1
coder5707 <gamingarts194@gmail.com>
2025-05-30 10:29:31 +0530

    initial commit'

--- App.tsx
+++ App.tsx
@@ -1,2 +1,34 @@
 import React from "react";
 import { View  } from "react-native";
+import Welcome from'./welcome';
+import Register_pg from "./login";
+import Focus from "./useref";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Dash from "./dash";
+import Login_pg from "./login2"
+import Id from "./id";
+import studentinfo from "./data";
+export type RootStackParamlist={
+  register:undefined;
+  home:undefined;
+  new_pg:undefined;
+}
+
+ const Stack=createNativeStackNavigator<RootStackParamlist>();
+
+const App = () => {
+   
+return(
+ <NavigationContainer>
+      <Stack.Navigator initialRouteName="register">
+        <Stack.Screen name="register" component={Register_pg} />
+        <Stack.Screen name="home" component={Dash} />
+         <Stack.Screen name="new_pg" component={Login_pg} />
+         <Stack.Screen name="studentinfo" component={studentinfo}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+);
+};
+export default App;
+
